1abdb76a95cd41d8947e7797e156ec3fe63baf6e
윤무걱정 <dbstjd3201@naver.com>
2022-04-22 18:01:35 +0900

    2022- 04 -22 Tv 풀것

---
+++ src/com/javex/ex08/TV.java
@@ -0,0 +1,24 @@
+package com.javex.ex08;
+
+public class TV {
+
+	//필드
+	private int channel;
+	private int volume;
+	private boolean power;
+
+	//생성자
+	public TV() {}
+	
+	public TV(int channel, int volume, boolean power) {
+		
+	}
+	
+	
+	//GS
+	
+	
+	//일반
+	
+	
+}
---
+++ src/com/javex/ex08/TVApp.java
@@ -0,0 +1,8 @@
+package com.javex.ex08;
+
+public class TVApp {
+
+	public static void main(String[] args) {
+		
+	}
+}
